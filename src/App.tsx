dbb71d033dd63bead3444203a96b5f807b8048ff
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2020-10-18 22:22:15 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Routes from './routes'
+
+import './styles/global.css'
+import 'leaflet/dist/leaflet.css'
+
+
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
